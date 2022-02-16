28bd6156eb3ed67a08e06a516fdb6d87a57c25f6
jstaylor926 <54418577+jstaylor926@users.noreply.github.com>
2022-02-16 07:49:48 -0500

    switch

---
+++ pages/mid.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Dashboard from "../components/Dashboard/Dashboard";
+
+const MidTerm = () => {
+    return (
+        <Dashboard>
+            <h1>Mid Term</h1>
+        </Dashboard>
+    )
+}
+
+export default MidTerm;
---
+++ pages/short.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Dashboard from "../components/Dashboard/Dashboard";
+
+const ShortTerm = () => {
+    return (
+        <Dashboard>
+            <h1>Short Term</h1>
+        </Dashboard>
+    )
+}
+
+export default ShortTerm;
